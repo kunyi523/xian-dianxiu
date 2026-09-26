90989984edaa89b82afe192fa92310f42093bb9e
红哥 <hongge@local>
2026-09-26 02:22:42 -0400

    劫云换回 PNG 版

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-DjUKmqRT.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-DjUKmqRT.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/home/hatch/workspace/xiantu/仙途点修/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-BtHKs5A4.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-BtHKs5A4.js"
+		} }]
+	},
+	"/": {
+		filePath: "/home/hatch/workspace/xiantu/仙途点修/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-B_dmrqaw.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
